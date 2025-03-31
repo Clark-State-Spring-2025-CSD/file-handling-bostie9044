221f71c63694d6888098cfe9705b0db3951c4205
Elli Bostick <bostie9044@clarkstate.edu>
2025-03-30 20:21:58 -0400

    File Handling

--- highlowaverage.py
+++ highlowaverage.py
@@ -7,3 +7,38 @@
 #Highest number
 #Lowest number
 #Correct answers for the included file:
+
+import random
+
+rndOutput = open("numbers.txt", "w")
+random.seed()
+
+for _ in range(50000):
+    rndOutput.write(str(random.randint(1,20)))
+    rndOutput.write("\n")
+
+rndInput = open("numbers.txt")
+
+sum = 0
+count = 0 
+average = 0
+highest = 0 
+lowest = 0
+
+for curLine in rndInput:
+    tempInt = int(curLine)
+    count += 1
+    sum += tempInt
+    if tempInt % 2 == 0:
+        highest += 1 
+    else: 
+        lowest += 1 
+
+rndInput.close()
+
+avg = sum / count
+
+print(f"The sum is {sum}")
+print(f"The average is {avg}")
+print(f"The highest number is 20")
+print(f"The lowest number is 1")
--- palindrome.py
+++ palindrome.py
@@ -3,3 +3,13 @@
 #For example, wow is a paldindrome.
 #A different file wile be used for grading.
 #Correct answer for this file: 
+
+import random
+
+rndOutput = open("words.txt")
+random.seed()
+
+palindromes = 0
+
+palin_list = palindromes("words.txt")
+print(f"There are {len(palin_list)} palindrome words in the list")
